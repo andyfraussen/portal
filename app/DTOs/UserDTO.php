4f3f99352d41dc2a87ec2d0deb3e0fc4ebecee8d
Andy Fraussen <andy.fraussen@eeveemobility.com>
2023-03-26 10:44:04 +0200

    fix userDTO

--- app/DTOs/UserDTO.php
+++ app/DTOs/UserDTO.php
@@ -7,19 +7,12 @@ class UserDTO
     public $id;
     public $name;
     public $email;
-    public $organisations;
 
     public function __construct($data = [])
     {
         $this->id = $data['id'];
         $this->name = $data['name'];
         $this->email = $data['email'];
-
-        // Load OrganisationDTO for each organization
-        $this->organisations = [];
-        foreach ($data['organisations'] as $organisation) {
-            $this->organisations[] = new OrganisationDTO($organisation);
-        }
     }
 
     public function toArray()
@@ -28,7 +21,6 @@ class UserDTO
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'organisations' => $this->organisations,
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\OrganisationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
-    Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::get('/users/{id}', [UserController::class, 'show']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
+Route::get('/organisations', [OrganisationController::class, 'index']);
+Route::post('/organisations', [OrganisationController::class, 'store']);
+Route::get('/organisations/{id}', [OrganisationController::class, 'show']);
+Route::put('/organisations/{id}', [OrganisationController::class, 'update']);
+Route::delete('/organisations/{id}', [OrganisationController::class, 'destroy']);
